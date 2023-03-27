c9e45668cc8de342c8c2018e87cf133797b21402
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2023-03-27 13:03:30 +0300

    add fragment_add.xml ui

--- app/src/main/java/com/example/todoaster/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoaster/fragments/list/ListFragment.kt
@@ -5,15 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.todoaster.R
+import com.example.todoaster.databinding.FragmentListBinding
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_add_item)
+        }
+
+        binding.noData.visibility = View.VISIBLE
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
